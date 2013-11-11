d9e5ba7cbde9cf8a8c702a6fb39417a9219ecb4e
Wayne Carter <wayne.carter@couchbase.com>
2013-11-11 10:54:24 -0800

    Refactor to allow running outside of Android.

---
+++ src/main/java/com/couchbase/cblite/storage/SQLException.java
@@ -0,0 +1,34 @@
+/**
+ * Created by Wayne Carter.
+ *
+ * Copyright (c) 2012 Couchbase, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+
+package com.couchbase.cblite.storage;
+
+public class SQLException extends RuntimeException {
+    public SQLException() {
+    }
+
+    public SQLException(String error) {
+        super(error);
+    }
+
+    public SQLException(String error, Throwable cause) {
+        super(error, cause);
+    }
+
+    public SQLException(Throwable cause) {
+        super(cause);
+    }
+}
