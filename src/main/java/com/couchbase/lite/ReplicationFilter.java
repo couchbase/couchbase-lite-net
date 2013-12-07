7cb82d61455334c4c7800d778f9255d78a6b44e7
Traun Leyden <traun@couchbase.com>
2013-12-06 18:12:50 -0800

    update comment

--- src/main/java/com/couchbase/lite/ReplicationFilter.java
+++ src/main/java/com/couchbase/lite/ReplicationFilter.java
@@ -10,7 +10,6 @@ import java.util.Map;
 public interface ReplicationFilter {
 
     // TODO: this needs to take a SavedRevision as a parameter, however
-    // TODO: the SavedRevision class does not exist yet on either Android or iOS
     boolean filter(RevisionInternal revision, Map<String, Object> params);
 
 
