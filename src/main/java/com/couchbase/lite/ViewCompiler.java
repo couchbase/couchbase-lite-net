a40b00c76fad2bd1d5aca5bc9cb901b890f87967
Traun Leyden <traun@couchbase.com>
2014-01-22 14:38:54 -0800

    Improve javadocs + @excludes for View and Attachment

--- src/main/java/com/couchbase/lite/ViewCompiler.java
+++ src/main/java/com/couchbase/lite/ViewCompiler.java
@@ -7,9 +7,23 @@ import com.couchbase.lite.internal.InterfaceAudience;
  */
 public interface ViewCompiler {
 
+    /**
+     * Compiles source code into a MapDelegate.
+     *
+     * @param mapSource The source code to compile into a Mapper.
+     * @param language The language of the source.
+     * @return A compiled Mapper.
+     */
     @InterfaceAudience.Public
     Mapper compileMap(String mapSource, String language);
 
+    /**
+     * Compiles source code into a ReduceDelegate.
+     *
+     * @param reduceSource The source code to compile into a Reducer.
+     * @param language The language of the source.
+     * @return A compiled Reducer
+     */
     @InterfaceAudience.Public
     Reducer compileReduce(String reduceSource, String language);
 
