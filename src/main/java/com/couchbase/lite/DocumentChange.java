821cffd6b71166a2ae74a70681d86c794a8607f7
Traun Leyden <traun@couchbase.com>
2014-01-22 12:42:30 -0800

    Javadocs and @exclude for DocumentChange

--- src/main/java/com/couchbase/lite/DocumentChange.java
+++ src/main/java/com/couchbase/lite/DocumentChange.java
@@ -5,8 +5,14 @@ import com.couchbase.lite.internal.InterfaceAudience;
 
 import java.net.URL;
 
+/**
+ * Provides details about a Document change.
+ */
 public class DocumentChange {
 
+    /**
+     * @exclude
+     */
     @InterfaceAudience.Private
     DocumentChange(RevisionInternal addedRevision, RevisionInternal winningRevision, boolean isConflict, URL sourceUrl) {
         this.addedRevision = addedRevision;
@@ -20,36 +26,51 @@ public class DocumentChange {
     private boolean isConflict;
     private URL sourceUrl;
 
+    @InterfaceAudience.Public
     public String getDocumentId() {
         return addedRevision.getDocId();
     }
 
+    @InterfaceAudience.Public
     public String getRevisionId() {
         return addedRevision.getRevId();
     }
 
+    @InterfaceAudience.Public
     public boolean isCurrentRevision() {
         return winningRevision != null && addedRevision.getRevId().equals(winningRevision.getRevId());
     }
 
+    @InterfaceAudience.Public
     public boolean isConflict() {
         return isConflict;
     }
 
+    @InterfaceAudience.Public
     public URL getSourceUrl() {
         return sourceUrl;
     }
 
+    /**
+     * @exclude
+     */
     @InterfaceAudience.Private
     public RevisionInternal getAddedRevision() {
         return addedRevision;
     }
 
+    /**
+     * @exclude
+     */
     @InterfaceAudience.Private
     RevisionInternal getWinningRevision() {
         return winningRevision;
     }
 
+    /**
+     * @exclude
+     */
+    @InterfaceAudience.Private
     public static DocumentChange tempFactory(RevisionInternal revisionInternal, URL sourceUrl, boolean inConflict) {
 
         DocumentChange change = new DocumentChange(
