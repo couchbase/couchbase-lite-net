b30211df3125476b09b088826fbb158d921d53d6
Traun Leyden <traun@couchbase.com>
2014-01-24 13:22:05 -0800

    ViewCompiler: mapSource & reduceSource args should just be named source

--- src/main/java/com/couchbase/lite/ViewCompiler.java
+++ src/main/java/com/couchbase/lite/ViewCompiler.java
@@ -10,21 +10,21 @@ public interface ViewCompiler {
     /**
      * Compiles source code into a MapDelegate.
      *
-     * @param mapSource The source code to compile into a Mapper.
+     * @param source The source code to compile into a Mapper.
      * @param language The language of the source.
      * @return A compiled Mapper.
      */
     @InterfaceAudience.Public
-    Mapper compileMap(String mapSource, String language);
+    Mapper compileMap(String source, String language);
 
     /**
      * Compiles source code into a ReduceDelegate.
      *
-     * @param reduceSource The source code to compile into a Reducer.
+     * @param source The source code to compile into a Reducer.
      * @param language The language of the source.
      * @return A compiled Reducer
      */
     @InterfaceAudience.Public
-    Reducer compileReduce(String reduceSource, String language);
+    Reducer compileReduce(String source, String language);
 
 }
