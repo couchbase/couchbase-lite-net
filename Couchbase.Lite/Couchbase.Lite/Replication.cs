f89f905e007310f781199eee9a758ec6fbfd312b
Pasin Suriyentrakorn <pasin@couchbase.com>
2014-04-14 17:06:16 -0700

    Fixed testPuller failed issue
    Fixed the testPuller failed issue caused by setting NULL TaskScheduler to the Task.ContinueWith() method when setting the LastSequence attribute to the Replication object.

--- Couchbase.Lite/Couchbase.Lite/Replication.cs
+++ Couchbase.Lite/Couchbase.Lite/Replication.cs
@@ -198,7 +198,7 @@ namespace Couchbase.Lite
                             .ContinueWith(task => 
                             {
                                 SaveLastSequence();
-                            }, null);
+                            });
                     }
                 }
             }
