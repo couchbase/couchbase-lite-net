c74315adbb1ae204d934bb770f783465a3409150
Tiago Margalho <tiagomargalho@gmail.com>
2014-10-26 22:03:44 +0000

    Added two tests that check whether calling the RunInTransaction method in the Database class commits or rollbacks the transaction depending on the delegate return value.

--- src/Couchbase.Lite.Tests.Shared/DatabaseTest.cs
+++ src/Couchbase.Lite.Tests.Shared/DatabaseTest.cs
@@ -414,6 +414,44 @@ namespace Couchbase.Lite
             database.WinningRevIDOfDoc(docNumericId, outIsDeleted, outIsConflict);
             Assert.IsTrue(outIsConflict.Count > 0);
         }
+
+        [Test]
+        [Description("If the delegate returns true, the transaction should be committed.")]
+        public void TestRunInTransactionCommits()
+        {
+            var storageEngine = database.StorageEngine;
+
+            storageEngine.RawQuery("CREATE TABLE transTest (id INTEGER PRIMARY KEY, whatever INTEGER)");
+
+            database.RunInTransaction(() =>
+            {
+                storageEngine.RawQuery("INSERT INTO transTest VALUES (0,1)");
+                return true;
+            });
+
+            var result = storageEngine.RawQuery("SELECT EXISTS (SELECT 1 FROM transTest WHERE id=0 AND whatever=1)");
+
+            Assert.AreEqual(1, result.GetInt(0));
+        }
+
+        [Test]
+        [Description("If the delegate returns false, the transaction should be rolledback.")]
+        public void TestRunInTransactionRollsback()
+        {
+            var storageEngine = database.StorageEngine;
+
+            storageEngine.RawQuery("CREATE TABLE transTest (id INTEGER PRIMARY KEY, whatever INTEGER)");
+
+            database.RunInTransaction(() =>
+            {
+                storageEngine.RawQuery("INSERT INTO transTest values (0,1)");
+                return false;
+            });
+
+            var result = storageEngine.RawQuery("SELECT EXISTS (SELECT 1 FROM transTest WHERE id=0 AND whatever=1)");
+
+            Assert.AreEqual(0, result.GetInt(0));
+        }
     }
 }
 
