07b2006f230cb1e878ad2961708e6a7309effb83
Jim Borden <jim.borden@couchbase.com>
2015-05-20 11:19:25 +0900

    Added unity demo project to samples

---
+++ samples/UnityDemo/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class CameraMove : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		float xAxisValue = CrossPlatformInputManager.GetAxis("Horizontal");
+		float zAxisValue = CrossPlatformInputManager.GetAxis("Vertical");
+		float turnAxisValue = CrossPlatformInputManager.GetAxis ("Turn");
+		if(Camera.current != null)
+		{
+			Camera.current.transform.Translate(new Vector3(xAxisValue * 0.2f, 0.0f, zAxisValue * 0.2f));
+			Camera.current.transform.Rotate(new Vector3(0.0f, turnAxisValue, 0.0f));
+		}
+	}
+}
---
+++ samples/UnityDemo/Assets/Scripts/CouchbaseSphere.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using Couchbase.Lite;
+using System.Collections.Generic;
+
+public class CouchbaseSphere : CouchbaseObject {
+
+	protected override bool IsDirty {
+		get {
+			return transform.hasChanged;
+		}
+	}
+
+	void OnMouseUp()
+	{
+		GetComponent<CouchbaseSphere> ().SaveToDocument ();
+	}
+
+	void Awake()
+	{
+		MeshRenderer gameObjectRenderer = gameObject.GetComponent<MeshRenderer>();
+		gameObjectRenderer.material = new Material (Shader.Find ("Diffuse"));
+		var rand = new System.Random();
+		gameObjectRenderer.material.color = new Color((float)rand.NextDouble(), (float)rand.NextDouble(), (float)rand.NextDouble());
+	}
+
+	protected override void RestoreFromDocumentProperties (IDictionary<string, object> props)
+	{
+		RestoreBaseData (props, SaveData.Position | SaveData.Color);
+	}
+
+	protected override bool UpdateRevision (UnsavedRevision rev)
+	{
+		rev.Properties ["type"] = "CouchbaseSphere";
+		EmitBaseData (rev, SaveData.Position | SaveData.Color);
+		return true;
+	}
+
+}
