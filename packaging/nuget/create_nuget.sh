0a212a46442921a76056e6c69a67b65b5cb39bfb
Zachary Gramana <z.gramana@me.com>
2015-06-18 15:44:38 -0700

    Updated nuspec files.

--- packaging/nuget/create_nuget.sh
+++ packaging/nuget/create_nuget.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 nuget pack -BasePath ../.. couchbase-lite.nuspec 
 nuget pack -BasePath ../.. couchbase-lite-listener.nuspec 
+nuget pack -BasePath ../.. couchbase-lite-listener-bonjour.nuspec 
