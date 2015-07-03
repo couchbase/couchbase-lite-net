a54492faeec1bf3d376620bb76f1c96868c929ce
Jim Borden <jim.borden@couchbase.com>
2015-07-03 18:36:11 +0900

    Bug fix in GitVersion.exe where it throws an exception if the user downloads the source through something other than Git (duh...).  This happens if they download a zip bundle from github (along with this change I also changed the README in the previous commit asking them not to do that because it's valuable in tracking down which commit they built from).  Also, I threw the source up with it, since it is so simple.

---
+++ Tools/GitVersion/Program.cs
@@ -0,0 +1,52 @@
+﻿//
+//  Program.cs
+//
+//  Author:
+//  	Jim Borden  <jim.borden@couchbase.com>
+//
+// Copyright (c) 2015 Couchbase, Inc.
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+using System;
+using System.IO;
+
+using GitSharp;
+
+namespace GitVersion
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            if(args.Length < 2) {
+                Console.Error.WriteLine("Not enough arguments!  Must specify an input directory and output file.");
+                return;
+            }
+            
+            string hash = "No git information";
+            if(Repository.IsValid(args[0])) {
+                var repo = new Repository(args[0]);
+                hash = repo.Head.CurrentCommit.ShortHash;
+            }
+            
+            File.WriteAllText(args[1], hash);
+        }
+    }
+}
