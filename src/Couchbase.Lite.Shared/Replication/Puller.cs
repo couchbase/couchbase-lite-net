7fb040af37416966de32aeb18530e7e4c08ef213
Jim Borden <jim.borden@couchbase.com>
2015-07-21 06:27:39 +0900

    Fix introduced NRE from last commit

--- src/Couchbase.Lite.Shared/Replication/Puller.cs
+++ src/Couchbase.Lite.Shared/Replication/Puller.cs
@@ -115,7 +115,9 @@ namespace Couchbase.Lite.Replicator
             _changeTracker = new ChangeTracker(RemoteUrl, mode, LastSequence, true, this, WorkExecutor);
             _changeTracker.Authenticator = Authenticator;
             if (ServerType.StartsWith("CouchDB")) {
-                _changeTracker.SetDocIDs(DocIds.ToList());
+                if(DocIds != null) {
+                    _changeTracker.SetDocIDs(DocIds.ToList());
+                }
             } else {
                 Log.W(TAG, "DocIds parameter only supported on CouchDB");
             }
