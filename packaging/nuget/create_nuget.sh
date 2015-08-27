569542eea1efe4bac0f2db9e28e15045c5bf8993
Jim Borden <jim.borden@couchbase.com>
2015-08-27 12:03:20 +0900

    Update packaging / version numbers and such

--- packaging/nuget/create_nuget.sh
+++ packaging/nuget/create_nuget.sh
@@ -1,4 +1,36 @@
 #!/bin/sh
+
+LITE_NUGET_DIR="../../src/Couchbase.Lite.Net35/bin/Nuget/"
+LITE_RELEASE_DIR="../../src/Couchbase.Lite.Net35/bin/Release"
+LITE_RELEASE_FILES="$LITE_RELEASE_DIR/Couchbase.Lite.*
+$LITE_RELEASE_DIR/ICSharpCode.SharpZipLib.dll
+$LITE_RELEASE_DIR/Microsoft.Scripting.Core.dll
+$LITE_RELEASE_DIR/Rackspace.Threading.dll*
+$LITE_RELEASE_DIR/Stateless.dll
+$LITE_RELEASE_DIR/SQLitePCL.raw.dll
+$LITE_RELEASE_DIR/System.Net.Http.Net35.dll*
+$LITE_RELEASE_DIR/System.Threading.Tasks.Net35.dll*
+$LITE_RELEASE_DIR/ugly_net35.dll"
+
+LITE_LISTENER_NUGET_DIR="../../src/ListenerComponent/Couchbase.Lite.Listener.Net35/bin/Nuget/"
+LITE_LISTENER_RELEASE_DIR="../../src/ListenerComponent/Couchbase.Lite.Listener.Net35/bin/Release"
+LITE_LISTENER_RELEASE_FILES="$LITE_LISTENER_RELEASE_DIR/Couchbase.Lite.Listener.dll*
+$LITE_LISTENER_RELEASE_DIR/Jint.dll"
+
+rm -rf $LITE_NUGET_DIR
+mkdir -p $LITE_NUGET_DIR
+for f in $LITE_RELEASE_FILES
+do
+  cp $f $LITE_NUGET_DIR
+done
+
+rm -rf $LITE_LISTENER_NUGET_DIR
+mkdir -p $LITE_LISTENER_NUGET_DIR
+for f in $LITE_LISTENER_RELEASE_FILES
+do
+  cp $f $LITE_LISTENER_NUGET_DIR
+done
+
 nuget pack -BasePath ../.. couchbase-lite.nuspec 
 nuget pack -BasePath ../.. couchbase-lite-listener.nuspec 
 nuget pack -BasePath ../.. couchbase-lite-listener-bonjour.nuspec 
