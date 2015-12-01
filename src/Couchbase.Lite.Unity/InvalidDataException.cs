62fc81060063b4a81df482cf16b3b80e62380acb
Jim Borden <jim.borden@couchbase.com>
2015-12-01 08:50:21 -0500

    Changes required to build for AOT (InvalidDataException and ThreadPool.UnsafeQueueWorkItem are missing)

---
+++ src/Couchbase.Lite.Unity/InvalidDataException.cs
@@ -0,0 +1,45 @@
+﻿//
+// System.InvalidDataException.cs
+//
+// Authors:
+//   Christopher James Lahey <clahey@ximian.com>
+//   Andreas Nahr (ClassDevelopment@A-SoftTech.com)
+//
+// Copyright (C) 2004, 2006 Novell, Inc (http://www.novell.com)
+//
+
+using System.Globalization;
+using System.Runtime.Serialization;
+
+namespace System.IO.Couchbase
+{
+    [Serializable]
+    public sealed class InvalidDataException : SystemException
+    {
+        const int Result = unchecked ((int)0x80131503);
+
+        // Constructors
+        public InvalidDataException ()
+            : base ("Invalid data format.")
+        {
+            HResult = Result;
+        }
+
+        public InvalidDataException (string message)
+            : base (message)
+        {
+            HResult = Result;
+        }
+
+        public InvalidDataException (string message, Exception innerException)
+            : base (message, innerException)
+        {
+            HResult = Result;
+        }
+
+        private InvalidDataException (SerializationInfo info, StreamingContext context)
+            : base (info, context)
+        {
+        }
+    }
+}
