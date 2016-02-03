b1fcc77f7504b03723a5d1014176ad646e4eb2e7
Jim Borden <jim.borden@couchbase.com>
2016-02-04 08:59:08 +0900

    Update the nuget package script to include the native storage engine components in the build

--- packaging/nuget/create_nuget.sh
+++ packaging/nuget/create_nuget.sh
@@ -1,5 +1,6 @@
 #/bin/sh
 
+#Do a thorough cleaning
 rm -rf ../../src/Couchbase.Lite.Net45/bin
 rm -rf ../../src/Couchbase.Lite.Net45/obj
 rm -rf ../../src/Couchbase.Lite.Net35/bin
@@ -43,11 +44,24 @@ rm -rf ../../src/StorageEngines/ForestDB/storage.forestdb.net35/obj
 rm -rf ../../src/StorageEngines/ForestDB/storage.forestdb.ios/obj
 rm -rf ../../src/StorageEngines/ForestDB/storage.forestdb.droid/obj
 
+#Build te managed DLLs
 xbuild /p:Configuration=Release ../../src/ListenerComponent/Bonjour/Couchbase.Lite.Listener.Bonjour.Net35/Couchbase.Lite.Listener.Bonjour.Net35.csproj
 xbuild /p:Configuration=Release ../../src/ListenerComponent/Bonjour/Couchbase.Lite.Listener.Bonjour.Net45/Couchbase.Lite.Listener.Bonjour.Net45.csproj
 xbuild /p:Configuration=Release ../../src/ListenerComponent/Bonjour/Couchbase.Lite.Listener.Bonjour.iOS/Couchbase.Lite.Listener.Bonjour.iOS.csproj
 xbuild /p:Configuration=Release ../../src/ListenerComponent/Bonjour/Couchbase.Lite.Listener.Bonjour.Android/Couchbase.Lite.Listener.Bonjour.Android.csproj
 
+#Build the storage engine native components
+xbuild /p:Configuration=Release ../../src/StorageEngines/SystemSQLite/storage.systemsqlite.net35/storage.systemsqlite.net35.csproj
+xbuild /p:Configuration=Release ../../src/StorageEngines/SystemSQLite/storage.systemsqlite.ios/storage.systemsqlite.ios.csproj
+xbuild /p:Configuration=Release ../../src/StorageEngines/SystemSQLite/storage.systemsqlite.droid/storage.systemsqlite.droid.csproj
+xbuild /p:Configuration=Release ../../src/StorageEngines/SQLCipher/storage.sqlcipher.net35/storage.sqlcipher.net35.csproj
+xbuild /p:Configuration=Release ../../src/StorageEngines/SQLCipher/storage.sqlcipher.ios/storage.sqlcipher.ios.csproj
+xbuild /p:Configuration=Release ../../src/StorageEngines/SQLCipher/storage.sqlcipher.droid/storage.sqlcipher.droid.csproj
+xbuild /p:Configuration=Release ../../src/StorageEngines/ForestDB/storage.forestdb.net35/storage.forestdb.net35.csproj
+xbuild /p:Configuration=Release ../../src/StorageEngines/ForestDB/storage.forestdb.ios/storage.forestdb.ios.csproj
+xbuild /p:Configuration=Release ../../src/StorageEngines/ForestDB/storage.forestdb.droid/storage.forestdb.droid.csproj
+
+#Copy the needed files for .NET 3.5 into a new directory to make things easier
 LITE_NUGET_DIR="../../src/Couchbase.Lite.Net35/bin/Nuget/"
 LITE_RELEASE_DIR="../../src/Couchbase.Lite.Net35/bin/Release"
 LITE_RELEASE_FILES="$LITE_RELEASE_DIR/Couchbase.Lite.*
@@ -80,6 +94,7 @@ do
   cp $f $LITE_LISTENER_NUGET_DIR
 done
 
+#Do the actual packing
 nuget pack -BasePath ../.. couchbase-lite.nuspec 
 nuget pack -BasePath ../.. couchbase-lite-listener.nuspec 
 nuget pack -BasePath ../.. couchbase-lite-listener-bonjour.nuspec 
