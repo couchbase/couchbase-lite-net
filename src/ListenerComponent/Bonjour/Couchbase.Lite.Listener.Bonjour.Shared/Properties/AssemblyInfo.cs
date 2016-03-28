b11f2a131309ab53f92b6d4a7cfee0bb71d3ceb9
Jim Borden <jim.borden@couchbase.com>
2016-03-29 08:12:34 +0900

    AssemblyInfo bump

--- src/ListenerComponent/Bonjour/Couchbase.Lite.Listener.Bonjour.Shared/Properties/AssemblyInfo.cs
+++ src/ListenerComponent/Bonjour/Couchbase.Lite.Listener.Bonjour.Shared/Properties/AssemblyInfo.cs
@@ -36,8 +36,8 @@ using System.Runtime.CompilerServices;
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion ("1.2.0.0")]
-[assembly: AssemblyInformationalVersion("1.2")] // Used by nuget for semver detection.
+[assembly: AssemblyVersion ("1.2.1.0")]
+[assembly: AssemblyInformationalVersion("1.2.1")] // Used by nuget for semver detection.
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
