422dd57c89dd23a8d9bab2631f874974cce43896
Jim Borden <jim.borden@couchbase.com>
2016-04-05 20:46:03 +0900

    Revert "Optimisation for deleting a view index"
    This reverts commit c3955c3f2521a63b07881c5616d45dda8fac4d9f.

--- src/StorageEngines/SQLiteCommon/storage.sqlite.common/src/SqliteViewStore.cs
+++ src/StorageEngines/SQLiteCommon/storage.sqlite.common/src/SqliteViewStore.cs
@@ -663,9 +663,7 @@ namespace Couchbase.Lite.Storage.SQLCipher
                             if (last == 0) {
                                 try {
                                     // If the lastSequence has been reset to 0, make sure to remove all map results:
-									changes = db.StorageEngine.RawQuery(view.QueryString("SELECT COUNT(*) FROM maps_#")).GetInt(0);
-                                    db.StorageEngine.ExecSQL(view.QueryString("DROP TABLE 'maps_#'"));
-									CreateIndex();
+                                    changes = db.StorageEngine.ExecSQL(view.QueryString("DELETE FROM 'maps_#'"));
                                 } catch (Exception) {
                                     ok = false;
                                 }
