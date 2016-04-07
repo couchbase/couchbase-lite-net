19aa60ed415e748f16655a95f2a2ca6e02551f50
Eric Levine <eric.levine@apx-labs.com>
2016-04-07 13:32:31 -0400

    Got the build script to create a nuget package which only contains the .Net and Android builds

--- script/build.fsx
+++ script/build.fsx
@@ -3,12 +3,17 @@
 
 open Fake
 open Fake.AssemblyInfoFile
+open System.IO
+open System
 
 let buildMode = "Release"
 let setParams defaults =
         { defaults with
             Verbosity = Some(Quiet)
-            Targets = ["Build"]
+            Targets = [
+                        "Clean"
+                        "Rebuild"
+                      ]
             Properties =
                 [
                     "Optimize", "True"
@@ -16,15 +21,37 @@ let setParams defaults =
                     "Configuration", buildMode
                 ]
          }
+
 let net45Solution = "src"</>"Couchbase.Lite.Net45.sln"
 let net45Project = "src"</>"Couchbase.Lite.Net45"</>"Couchbase.Lite.Net45.csproj"
+let androidProject = "src"</>"Couchbase.Lite.Android"</>"Couchbase.Lite.Android.csproj"
 let cbForestProject = "src"</>"Couchbase.Lite.Shared"</>"vendor"</>"cbforest"</>"CSharp"</>"cbforest-sharp.Net45"</>"cbforest-sharp.Net45.csproj"
+
+let nugetPath = Path.GetFullPath(@"./src/.nuget/Nuget.Exe")
+let nuspecPath = Path.GetFullPath(@"./packaging/nuget/couchbase-lite-apx.nuspec")
+let artifactsNuGetDir = Path.GetFullPath(@"./artifacts/nuget/")
+let artifactsBuildDir = Path.GetFullPath(@"./artifacts/build/")
 
 Target "Build" (fun _ ->
     build setParams cbForestProject|> DoNothing    
     build setParams net45Project |> DoNothing
+    build setParams androidProject |> DoNothing
+ 
+)
+
+Target "Package" (fun _ ->
+    ensureDirectory artifactsNuGetDir
+    ensureDirectory artifactsBuildDir
+    let commandArgs = sprintf @"pack -BasePath . -Verbosity detailed -OutputDirectory %s %s" artifactsNuGetDir nuspecPath
+    Console.WriteLine(commandArgs)
+    let result = Shell.Exec(nugetPath,  commandArgs)
+    if result <> 0 then failwithf "%s exited with error %d" "build.bat" result
 )
 
 
+// Dependencies
+"Build"
+     ==> "Package"
+
 // start build
-RunTargetOrDefault "Build"
+RunTargetOrDefault "Package"
