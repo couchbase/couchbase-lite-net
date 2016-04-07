13a3f8cbec3a67ffd22012ed8f8efd121ee8f197
Eric Levine <eric.levine@apx-labs.com>
2016-04-07 13:34:34 -0400

    A little bit of cleanup

--- script/build.fsx
+++ script/build.fsx
@@ -22,11 +22,13 @@ let setParams defaults =
                 ]
          }
 
+// solution and project paths
 let net45Solution = "src"</>"Couchbase.Lite.Net45.sln"
 let net45Project = "src"</>"Couchbase.Lite.Net45"</>"Couchbase.Lite.Net45.csproj"
 let androidProject = "src"</>"Couchbase.Lite.Android"</>"Couchbase.Lite.Android.csproj"
 let cbForestProject = "src"</>"Couchbase.Lite.Shared"</>"vendor"</>"cbforest"</>"CSharp"</>"cbforest-sharp.Net45"</>"cbforest-sharp.Net45.csproj"
 
+// nuget related paths
 let nugetPath = Path.GetFullPath(@"./src/.nuget/Nuget.Exe")
 let nuspecPath = Path.GetFullPath(@"./packaging/nuget/couchbase-lite-apx.nuspec")
 let artifactsNuGetDir = Path.GetFullPath(@"./artifacts/nuget/")
@@ -36,15 +38,16 @@ Target "Build" (fun _ ->
     build setParams cbForestProject|> DoNothing    
     build setParams net45Project |> DoNothing
     build setParams androidProject |> DoNothing
- 
 )
 
 Target "Package" (fun _ ->
     ensureDirectory artifactsNuGetDir
     ensureDirectory artifactsBuildDir
+    
+    // create and execute the command ourselves, since the NuGet helper doesn't allow us to set the BasePath command argument which we need
     let commandArgs = sprintf @"pack -BasePath . -Verbosity detailed -OutputDirectory %s %s" artifactsNuGetDir nuspecPath
-    Console.WriteLine(commandArgs)
     let result = Shell.Exec(nugetPath,  commandArgs)
+    
     if result <> 0 then failwithf "%s exited with error %d" "build.bat" result
 )
 
