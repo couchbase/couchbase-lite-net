598aa9b28cfd368233c7065c8d02c45effff4acc
Jim Borden <jim.borden@couchbase.com>
2017-03-01 14:37:48 +0900

    One more time with a hardcoded path

--- src/build/do_build.bat
+++ src/build/do_build.bat
@@ -10,5 +10,5 @@ if not exist ..\couchbase.snk (
 
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" Couchbase.Lite.csproj /t:Transform /p:TransformFile="Properties\DynamicAssemblyInfo.tt"
 dotnet restore
-dotnet build -c Packaging && sn -Ra bin\Packaging\netstandard1.4\Couchbase.Lite.dll ..\couchbase.snk
+dotnet build -c Packaging && "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\sn.exe" -Ra bin\Packaging\netstandard1.4\Couchbase.Lite.dll ..\couchbase.snk
 popd
