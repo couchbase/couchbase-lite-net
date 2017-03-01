aca1c0138b5ecc9b8b9d168bd18d9e1c8eade245
Jim Borden <jim.borden@couchbase.com>
2017-03-01 14:25:57 +0900

    Try to satisfy the build server by hard coding some paths..

--- src/build/do_build.bat
+++ src/build/do_build.bat
@@ -7,11 +7,8 @@ if not exist ..\couchbase.snk (
     popd
     exit /b 1
 )
-if not defined VSCMD_ARG_HOST_ARCH (
-    call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
-)
 
-msbuild Couchbase.Lite.csproj /t:Transform /p:TransformFile="Properties\DynamicAssemblyInfo.tt"
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" Couchbase.Lite.csproj /t:Transform /p:TransformFile="Properties\DynamicAssemblyInfo.tt"
 dotnet restore
 dotnet build -c Packaging && sn -Ra bin\Packaging\netstandard1.4\Couchbase.Lite.dll ..\couchbase.snk
 popd
