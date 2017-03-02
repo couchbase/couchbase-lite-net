418f0e8dd2c2db316a5b2822c78d0dfa4e64c0b3
Jim Borden <jim.borden@couchbase.com>
2017-03-02 13:39:16 +0900

    Modify build script to pass in the source of the CI nuget packages

--- src/build/do_build.bat
+++ src/build/do_build.bat
@@ -8,15 +8,21 @@ if not exist ..\couchbase.snk (
     exit /b 1
 )
 
+if not defined NUGET_REPO (
+    echo NUGET_REPO not defined, aborting...
+    popd
+    exit /b 1
+)
+
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" Couchbase.Lite.csproj /t:Transform /p:TransformFile="Properties\DynamicAssemblyInfo.tt"
-dotnet restore
+dotnet restore -s %NUGET_REPO%
 dotnet build -c Packaging
 
 pushd ..\Couchbase.Lite.Support.UWP
 if not exist ..\..\nuget.exe (
     powershell -Command "Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile ..\..\nuget.exe"
 )
-..\..\nuget.exe restore
+..\..\nuget.exe restore -Source %NUGET_REPO%
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" Couchbase.Lite.Support.UWP.csproj /p:Configuration=Packaging
 popd
 
