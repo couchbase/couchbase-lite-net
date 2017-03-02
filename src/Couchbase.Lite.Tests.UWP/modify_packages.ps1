b9a41adc9c3d57dc62e2abb5d0a55cef5357dee9
Jim Borden <jim.borden@couchbase.com>
2017-03-02 12:33:07 +0900

    Add in packages for build server to use to auto bump package versions

---
+++ src/Couchbase.Lite.Tests.UWP/modify_packages.ps1
@@ -0,0 +1,18 @@
+param (
+    [Parameter(Mandatory=$true)][string]$version
+)
+
+$scriptpath = Split-Path $MyInvocation.MyCommand.Path
+Push-Location $scriptpath
+[Environment]::CurrentDirectory = $scriptpath
+
+$files = "project.json","UWPHack\\project.json"
+foreach($file in $files) {
+    $content = cat $file | ConvertFrom-Json
+    foreach($key in ($content.dependencies | Get-Member Couchbase.Lite* -MemberType NoteProperty).Name) {
+        $content.dependencies.$key = $version
+    }
+
+    $text = ConvertTo-Json $content
+    [System.IO.File]::WriteAllText($file, $text)
+}
