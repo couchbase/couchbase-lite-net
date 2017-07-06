91b4a6395c92ddf9c2ca0f2c95386476ccd55a3d
Jim Borden <jim.borden@couchbase.com>
2017-07-06 11:33:11 +0900

    Correct the UWP nuspec and begin the painful process of fixing up Git's completely f-ed line endings

--- src/Couchbase.Lite.Tests.UWP/UnitTestApp.xaml.cs
+++ src/Couchbase.Lite.Tests.UWP/UnitTestApp.xaml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
