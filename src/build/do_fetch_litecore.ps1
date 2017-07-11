9f35ca694a527435eafc5158ee129339a4b81814
Jim Borden <jim.borden@couchbase.com>
2017-07-11 14:41:43 +0900

    Update the litecore fetching script to be more dynamic about what it gets

--- src/build/do_fetch_litecore.ps1
+++ src/build/do_fetch_litecore.ps1
@@ -1,17 +1,29 @@
 param(
-    [Parameter(Mandatory=$true)][string]$NexusRepo
+    [Parameter(Mandatory=$true)][string]$NexusRepo,
+    [Parameter(Mandatory=$true)][string[]]$Variants,
+    [switch]$DebugLib
 )
 
 pushd $PSScriptRoot\..\..\vendor\couchbase-lite-core\build_cmake
 $sha = $(& 'C:\Program Files\Git\bin\git.exe' rev-parse HEAD)
+$suffix = ""
+if($DebugLib) {
+    $suffix = "-debug"
+}
+
 Write-Host "Fetching variants for $sha..."
-$VARIANTS = @("macosx", "linux")
-$EXTENSIONS = @("zip", "tar.gz")
+if($Variants[0].ToLower() -eq "all") {
+    $Variants = @("macosx", "linux")
+}
+
+$VARIANT_EXT = @{"macosx" = "zip"; "linux" = "tar.gz"}
 try {
     $i = 0
-    foreach ($variant in $VARIANTS) {
+    foreach ($variant in $Variants) {
         echo "Fetching $variant..."
-        Invoke-WebRequest $NexusRepo/couchbase-litecore-$variant/$sha/couchbase-litecore-$variant-$sha.$($EXTENSIONS[$i]) -Out litecore-$variant.$($EXTENSIONS[$i++])
+        $extension = $VARIANT_EXT[$variant]
+        
+        Invoke-WebRequest $NexusRepo/couchbase-litecore-$variant/$sha/couchbase-litecore-$variant-$sha$suffix.$extension -Out litecore-$variant$suffix.$extension
     }
 } catch [System.Net.WebException] {
     popd
@@ -26,10 +38,16 @@ try {
     throw
 }
 
-& 7z e -y litecore-macosx.zip lib/libLiteCore.dylib
-& 7z x litecore-linux.tar.gz
-& 7z e -y litecore-linux.tar lib/*
-rm litecore-macosx.zip
-rm litecore-linux.tar
-rm litecore-linux.tar.gz
+if(Test-Path "litecore-macosx$suffix.zip"){
+    & 7z e -y litecore-macosx$suffix.zip lib/libLiteCore.dylib
+    rm litecore-macosx$suffix.zip
+}
+
+if(Test-Path "litecore-linux$suffix.tar.gz"){
+    & 7z x litecore-linux$suffix.tar.gz
+    & 7z e -y litecore-linux$suffix.tar lib/*
+    rm litecore-linux$suffix.tar
+    rm litecore-linux$suffix.tar.gz
+}
+
 popd
