3376c2f6735bb8f0069d63819e62db8d535a6caf
Jim Borden <jim.borden@couchbase.com>
2017-07-12 06:43:40 +0900

    Remove git functionality from script and just pass it the SHA

--- src/build/do_fetch_litecore.ps1
+++ src/build/do_fetch_litecore.ps1
@@ -1,18 +1,17 @@
 param(
     [Parameter(Mandatory=$true)][string]$NexusRepo,
     [Parameter(Mandatory=$true)][string[]]$Variants,
-    [string]$Git = "git",
+    [Parameter(Mandatory=$true)][string]$Sha,
     [switch]$DebugLib
 )
 
 pushd $PSScriptRoot\..\..\vendor\couchbase-lite-core\build_cmake
-$sha = $(& $Git rev-parse HEAD)
 $suffix = ""
 if($DebugLib) {
     $suffix = "-debug"
 }
 
-Write-Host "Fetching variants for $sha..."
+Write-Host "Fetching variants for $Sha..."
 if($Variants[0].ToLower() -eq "all") {
     $Variants = @("macosx", "linux")
 }
@@ -24,14 +23,14 @@ try {
         echo "Fetching $variant..."
         $extension = $VARIANT_EXT[$variant]
         
-        Invoke-WebRequest $NexusRepo/couchbase-litecore-$variant/$sha/couchbase-litecore-$variant-$sha$suffix.$extension -Out litecore-$variant$suffix.$extension
+        Invoke-WebRequest $NexusRepo/couchbase-litecore-$variant/$Sha/couchbase-litecore-$variant-$Sha$suffix.$extension -Out litecore-$variant$suffix.$extension
     }
 } catch [System.Net.WebException] {
     popd
     if($_.Exception.Status -eq [System.Net.WebExceptionStatus]::ProtocolError) {
         $res = $_.Exception.Response.StatusCode
         if($res -eq 404) {
-            Write-Host "$variant for $sha is not ready yet!"
+            Write-Host "$variant for $Sha is not ready yet!"
             exit 1
         }
     }
