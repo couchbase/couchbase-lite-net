04affed94d82597909c9cbf4bc793b5d4e9922ad
Jim Borden <jim.borden@couchbase.com>
2017-07-13 13:43:47 +0900

    Correct script because the test runner no longer has a Packaging target

--- src/Couchbase.Lite.Tests.UWP/run_tests.bat
+++ src/Couchbase.Lite.Tests.UWP/run_tests.bat
@@ -1,4 +1,4 @@
 @echo off
 
-msbuild /p:Configuration=Packaging;Platform=x64 Couchbase.Lite.Tests.UWP.csproj
-vstest.console.exe /InIsolation /Platform:x64 AppPackages\Couchbase.Lite.Tests.UWP_1.0.0.0_x64_Packaging_Test\Couchbase.Lite.Tests.UWP_1.0.0.0_x64_Packaging.appx /Logger:trx
+msbuild /p:Configuration=Release /p:Platform=x64 Couchbase.Lite.Tests.UWP.csproj
+vstest.console.exe /InIsolation /Platform:x64 AppPackages\Couchbase.Lite.Tests.UWP_1.0.0.0_x64_Test\Couchbase.Lite.Tests.UWP_1.0.0.0_x64.appx /Logger:trx
