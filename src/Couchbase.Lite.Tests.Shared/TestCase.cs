a1d429bceeb95edc5862b29e945bef169ceed19d
Jim Borden <jim.borden@couchbase.com>
2017-07-21 10:18:22 +0900

    Fix an embarassing spelling mistake

--- src/Couchbase.Lite.Tests.Shared/TestCase.cs
+++ src/Couchbase.Lite.Tests.Shared/TestCase.cs
@@ -66,7 +66,7 @@ namespace Test
 #if NETCOREAPP1_0
         static TestCase()
         {
-            Couchbase.Lite.Support.NetDestkop.Activate();
+            Couchbase.Lite.Support.NetDesktop.Activate();
         }
 #endif
 
