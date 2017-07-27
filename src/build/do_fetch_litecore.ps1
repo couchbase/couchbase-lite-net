327737d6710adfa6c6e1b000cd579a49c4aa4a19
Jim Borden <jim.borden@couchbase.com>
2017-07-27 15:32:49 +0900

    mv -> Move-Item

--- src/build/do_fetch_litecore.ps1
+++ src/build/do_fetch_litecore.ps1
@@ -63,8 +63,8 @@ if(Test-Path "litecore-macosx$suffix.zip"){
 if(Test-Path "litecore-linux$suffix.tar.gz"){
     & 7z x litecore-linux$suffix.tar.gz
     & 7z e -y litecore-linux$suffix.tar lib/libLiteCore.so lib/libsqlite3.so lib/libc++.so.1.0 lib/libc++abi.so.1.0
-    mv -Force libc++.so.1.0 libc++.so
-    mv -Force libc++abi.so.1.0 libc++abi.so
+    Move-Item -Force libc++.so.1.0 libc++.so
+    Move-Item -Force libc++abi.so.1.0 libc++abi.so
     rm litecore-linux$suffix.tar
     rm litecore-linux$suffix.tar.gz
 }
