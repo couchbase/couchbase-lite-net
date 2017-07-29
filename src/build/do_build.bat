014ea9cdd6111a08826caf65500630cc2eccb76c
Jim Borden <jim.borden@couchbase.com>
2017-07-29 22:09:13 +0900

    Fallback to the original Nuget package for SourceLink until PR situation is resolved

--- src/build/do_build.bat
+++ src/build/do_build.bat
@@ -44,27 +44,12 @@ echo *** COPYING NATIVE RESOURCES ***
 echo.
 if not exist ..\Couchbase.Lite.Support.Apple\iOS\Resources mkdir ..\Couchbase.Lite.Support.Apple\iOS\Resources
 xcopy /Y ..\..\vendor\couchbase-lite-core\build_cmake\ios-fat\libLiteCore.dylib ..\Couchbase.Lite.Support.Apple\iOS\Resources
-cd ..
 
 echo.
 echo *** BUILDING ***
 echo.
-echo %cd%
-
-if exist ..\Tools\SourceLink (
-    cd ..\Tools\SourceLink\dotnet-sourcelink-git 
-    dotnet restore
-    dotnet build -c Release dotnet-sourcelink-git.csproj
-    cd ..\..\..\src\Couchbase.Lite
-
-    del sourcelink.compile
-    del sourcelink.json
-    dotnet build -c Packaging /p:SourceLinkCreate=true
-    del sourcelink.json
-    dotnet ..\..\Tools\SourceLink\dotnet-sourcelink-git\bin\Release\netcoreapp1.0\dotnet-sourcelink-git.dll create --url "https://raw.githubusercontent.com/couchbase/couchbase-lite-net/{commit}/*"
-)
 
 cd ..
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" Couchbase.Lite.sln /p:Configuration=Packaging
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" Couchbase.Lite.sln /p:Configuration=Packaging /p:SourceLinkCreate=true
 
 popd
