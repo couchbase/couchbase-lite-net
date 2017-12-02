89b783f4b4b62d41489f457df4dff708a045a4b5
Jim Borden <jim.borden@couchbase.com>
2017-12-02 10:13:12 +0900

    Try to mitigate some Android silliness around Resources

--- src/Couchbase.Lite.Tests.iOS/modify_packages.sh
+++ src/Couchbase.Lite.Tests.iOS/modify_packages.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 pushd `dirname $0`
-sed "s/$1-b..../$1-$2/g" packages.config > tmp
-mv tmp packages.config
 sed "s/$1-b..../$1-$2/g" Couchbase.Lite.Tests.iOS.csproj > tmp
 mv tmp Couchbase.Lite.Tests.iOS.csproj
 popd
