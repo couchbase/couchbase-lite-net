3c2c2749f6ea9f4c796b92c257223068e8fe5f42
Jim Borden <jim.borden@couchbase.com>
2018-01-17 12:54:17 +0900

    Upgrade UWP unit test app to latest Windows 10 version, UWP 6+, and set up tests for the correct invocation

--- src/Couchbase.Lite.Tests.UWP/run_tests.bat
+++ src/Couchbase.Lite.Tests.UWP/run_tests.bat
@@ -1,4 +1,4 @@
 @echo off
 
 msbuild /p:Configuration=Release /p:Platform=x64 Couchbase.Lite.Tests.UWP.csproj
-vstest.console.exe /InIsolation /Platform:x64 AppPackages\Couchbase.Lite.Tests.UWP_1.0.0.0_x64_Test\Couchbase.Lite.Tests.UWP_1.0.0.0_x64.appx /Logger:trx
+vstest.console.exe /InIsolation /Platform:x64 /Framework:FrameworkUap10 bin\Release\Couchbase.Lite.Tests.UWP.build.appxrecipe /Logger:trx
