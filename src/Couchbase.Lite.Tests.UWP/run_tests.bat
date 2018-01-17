31a9fc32d77f3b39d74eea03542a2a505a7ad57e
Jim Borden <jim.borden@couchbase.com>
2018-01-17 14:29:35 +0900

    Fix scripting mistake for launching UWP tests

--- src/Couchbase.Lite.Tests.UWP/run_tests.bat
+++ src/Couchbase.Lite.Tests.UWP/run_tests.bat
@@ -1,4 +1,4 @@
 @echo off
 
 msbuild /p:Configuration=Release /p:Platform=x64 Couchbase.Lite.Tests.UWP.csproj
-vstest.console.exe /InIsolation /Platform:x64 /Framework:FrameworkUap10 bin\Release\Couchbase.Lite.Tests.UWP.build.appxrecipe /Logger:trx
+vstest.console.exe /InIsolation /Platform:x64 /Framework:FrameworkUap10 bin\x64\Release\Couchbase.Lite.Tests.UWP.build.appxrecipe /Logger:trx
