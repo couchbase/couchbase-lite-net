bf066a83aa2db14f0e607fb325a98911ea2b8b6c
Jim Borden <jim.borden@couchbase.com>
2018-04-10 13:06:38 +0900

    Fix modify package shell script to handle major version changes

--- src/Couchbase.Lite.Tests.Android/modify_packages.sh
+++ src/Couchbase.Lite.Tests.Android/modify_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pushd `dirname $0`
-sed "s/$1-b..../$1-$2/g" Couchbase.Lite.Tests.Android.csproj > tmp
+sed "s/[0-9].[0-9].[0-9]-b..../$1-$2/g" Couchbase.Lite.Tests.Android.csproj > tmp
 mv tmp Couchbase.Lite.Tests.Android.csproj
 popd
--- src/Couchbase.Lite.Tests.NetCore/modify_packages.sh
+++ src/Couchbase.Lite.Tests.NetCore/modify_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pushd `dirname $0`
-sed "s/$1-b..../$1-$2/g" Couchbase.Lite.Tests.NetCore.csproj > tmp
+sed "s/[0-9].[0-9].[0-9]-b..../$1-$2/g" Couchbase.Lite.Tests.NetCore.csproj > tmp
 mv tmp Couchbase.Lite.Tests.NetCore.csproj
 popd
