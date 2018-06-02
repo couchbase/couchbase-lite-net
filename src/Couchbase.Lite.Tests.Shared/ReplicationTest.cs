85c6450fd186e4a2ff99d430aa0cc5e03f55fb72
DESKTOP-6IIU16O\Sandy <sandy.chuang@couchbase.com>
2018-06-01 20:22:43 -0700

    #1020 Test Close Server When Replicator not existsed yet

--- src/Couchbase.Lite.Tests.Shared/ReplicationTest.cs
+++ src/Couchbase.Lite.Tests.Shared/ReplicationTest.cs
@@ -70,9 +70,35 @@ namespace Test
             ReopenDB();
             Database.Delete("otherdb", Directory);
             _otherDB = OpenDB("otherdb");
+            Database.SetLogLevel(LogDomain.Replicator, LogLevel.Verbose);
         }
 
-        #if COUCHBASE_ENTERPRISE
+        [Fact]
+        public async Task TestCloseServerWhenReplicatorExistsAsync()
+        {
+            var targetEndpoint = new URLEndpoint(new Uri("ws://192.168.0.11:4984/app"));
+            var config = new ReplicatorConfiguration(Db, targetEndpoint);
+            using (var repl = new Replicator(config))
+            {
+                repl.Start();
+                var count = 0;
+
+                this.Invoking(x => ReopenDB())
+                    .ShouldThrow<CouchbaseLiteException>(
+                        "because the database cannot be closed while replication is running");
+            
+                while (count++ > 20 && repl.Status.Activity != ReplicatorActivityLevel.Stopped)
+                {
+                    WriteLine($"Replication status still {repl.Status.Activity}, waiting for stopped...");
+                    await Task.Delay(500);
+                }
+
+                if(repl.Status.Activity == ReplicatorActivityLevel.Stopped)
+                    WriteLine($"Replication status still {repl.Status.Activity}, stopped");
+            }
+        }
+
+#if COUCHBASE_ENTERPRISE
         [Fact]
         public void TestReadOnlyConfiguration()
         {
