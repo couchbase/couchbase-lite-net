9c11aeece8d3eca1d390100ace61982b52a6a9a8
DESKTOP-6IIU16O\Sandy <sandy.chuang@couchbase.com>
2018-07-09 14:58:09 -0700

    Xamarin Android proxy support

---
+++ src/Couchbase.Lite.Support.Android/Support/XamarinAndroidProxy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Couchbase.Lite.DI;
+using Java.Lang;
+
+namespace Couchbase.Lite.Support.Android.Support
+{
+    public sealed class XamarinAndroidProxy : IProxy
+    {
+        public IWebProxy CreateProxy(Uri destination)
+        {
+            // if a proxy is enabled set it up here
+            string host = JavaSystem.GetProperty("http.proxyHost")?.TrimEnd('/');
+            string port = JavaSystem.GetProperty("http.proxyPort");
+
+            if (host == null)
+                return null;
+
+            //proxy auth
+            //ICredentials credentials = new NetworkCredential("username", "password");
+            //WebProxy proxy = new WebProxy(new Uri(host+':'+port), true, null, credentials);
+            
+            return new WebProxy(host, Int32.Parse(port));
+        }
+    }
+}
