2d496ea16316d717ec89a8e644710cf0549fe9a4
DESKTOP-6IIU16O\Sandy <sandy.chuang@couchbase.com>
2018-08-24 14:05:11 -0700

    update script for netcore

--- src/Couchbase.Lite.Tests.NetCore/modify_packages.ps1
+++ src/Couchbase.Lite.Tests.NetCore/modify_packages.ps1
@@ -1,7 +1,3 @@
-param (
-    [Parameter(Mandatory=$true)][string]$version,
-    [switch]$debugProject
-)
 $sourceProjectFile = "couchbase-lite-net/src/Couchbase.Lite.Tests.NetCore/Couchbase.Lite.Tests.NetCore.Source.csproj"
 if (-NOT (Test-Path $sourceProjectFile)) {
 
@@ -25,6 +21,7 @@ for($i = 0; $i -lt $content.Length; $i++) {
 
 [System.IO.File]::WriteAllLines("Couchbase.Lite.Tests.NetCore.csproj", $content)
 } else {
+param([Parameter(Mandatory=$true)][string]$version, [switch]$debugProject)
 
 $scriptpath = Split-Path $MyInvocation.MyCommand.Path
 Push-Location $scriptpath
