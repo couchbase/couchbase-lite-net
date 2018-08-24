f41e6f915092ced3639e99ab0a929c48f3e61f21
DESKTOP-6IIU16O\Sandy <sandy.chuang@couchbase.com>
2018-08-24 13:01:09 -0700

    update script for netcore

--- src/Couchbase.Lite.Tests.NetCore/modify_packages.ps1
+++ src/Couchbase.Lite.Tests.NetCore/modify_packages.ps1
@@ -1,3 +1,27 @@
+$sourceProjectFile = "couchbase-lite-net/src/Couchbase.Lite.Tests.NetCore/Couchbase.Lite.Tests.NetCore.Source.csproj"
+if (-NOT (Test-Path $sourceProjectFile)) {
+    param (
+    [Parameter(Mandatory=$true)][string]$version
+)
+
+$scriptpath = Split-Path $MyInvocation.MyCommand.Path
+Push-Location $scriptpath
+[Environment]::CurrentDirectory = $scriptpath
+
+$content = [System.IO.File]::ReadAllLines("Couchbase.Lite.Tests.NetCore.csproj")
+$regex = New-Object -TypeName "System.Text.RegularExpressions.Regex" ".*?<PackageReference Include=`"Couchbase.Lite.Enterprise.*?`" Version=`"(.*?)`""
+for($i = 0; $i -lt $content.Length; $i++) {
+    $line = $content[$i]
+    $matches = $regex.Matches($line)
+    if($matches.Count -gt 0) {
+        $oldVersion = $matches[0].Groups[1]
+        $line = $line.Replace($oldVersion, $version)
+        $content[$i] = $line
+    }
+}
+
+[System.IO.File]::WriteAllLines("Couchbase.Lite.Tests.NetCore.csproj", $content)
+} else {
 param (
     [Parameter(Mandatory=$true)][string]$version,
     [switch]$debugProject
@@ -28,4 +52,5 @@ if($debugProject) {
     [System.IO.File]::WriteAllLines("Couchbase.Lite.Tests.NetCore.Source.csproj", $content)
 } else {
     [System.IO.File]::WriteAllLines("Couchbase.Lite.Tests.NetCore.csproj", $content)
+}
 }
