465a0315e45f944aecfe1867df5d548d5269ffb5
DESKTOP-6IIU16O\Sandy <sandy.chuang@couchbase.com>
2018-08-27 12:30:04 -0700

    formatting net core's modify packages script and update uwp's modify packages script

--- src/Couchbase.Lite.Tests.NetCore/modify_packages.ps1
+++ src/Couchbase.Lite.Tests.NetCore/modify_packages.ps1
@@ -1,48 +1,48 @@
 param([Parameter(Mandatory=$true)][string]$version, [switch]$debugProject)
 $sourceProjectFile = "Couchbase.Lite.Tests.NetCore.Source.csproj"
 if (-NOT (Test-Path $sourceProjectFile)) {
-$scriptpath = Split-Path $MyInvocation.MyCommand.Path
-Push-Location $scriptpath
-[Environment]::CurrentDirectory = $scriptpath
+    $scriptpath = Split-Path $MyInvocation.MyCommand.Path
+    Push-Location $scriptpath
+    [Environment]::CurrentDirectory = $scriptpath
 
-$content = [System.IO.File]::ReadAllLines("Couchbase.Lite.Tests.NetCore.csproj")
-$regex = New-Object -TypeName "System.Text.RegularExpressions.Regex" ".*?<PackageReference Include=`"Couchbase.Lite.Enterprise.*?`" Version=`"(.*?)`""
-for($i = 0; $i -lt $content.Length; $i++) {
-    $line = $content[$i]
-    $matches = $regex.Matches($line)
-    if($matches.Count -gt 0) {
-        $oldVersion = $matches[0].Groups[1]
-        $line = $line.Replace($oldVersion, $version)
-        $content[$i] = $line
-    }
-}
-
-[System.IO.File]::WriteAllLines("Couchbase.Lite.Tests.NetCore.csproj", $content)
-} else {
-$scriptpath = Split-Path $MyInvocation.MyCommand.Path
-Push-Location $scriptpath
-[Environment]::CurrentDirectory = $scriptpath
-
-if($debugProject) {
-    $content = [System.IO.File]::ReadAllLines("Couchbase.Lite.Tests.NetCore.Source.csproj")
-} else {
     $content = [System.IO.File]::ReadAllLines("Couchbase.Lite.Tests.NetCore.csproj")
-}
-
-$regex = New-Object -TypeName "System.Text.RegularExpressions.Regex" ".*?<PackageReference Include=`"Couchbase.Lite.Enterprise.*?`" Version=`"(.*?)`""
-for($i = 0; $i -lt $content.Length; $i++) {
-    $line = $content[$i]
-    $matches = $regex.Matches($line)
-    if($matches.Count -gt 0) {
-        $oldVersion = $matches[0].Groups[1]
-        $line = $line.Replace($oldVersion, $version)
-        $content[$i] = $line
+    $regex = New-Object -TypeName "System.Text.RegularExpressions.Regex" ".*?<PackageReference Include=`"Couchbase.Lite.Enterprise.*?`" Version=`"(.*?)`""
+    for($i = 0; $i -lt $content.Length; $i++) {
+        $line = $content[$i]
+        $matches = $regex.Matches($line)
+        if($matches.Count -gt 0) {
+            $oldVersion = $matches[0].Groups[1]
+            $line = $line.Replace($oldVersion, $version)
+            $content[$i] = $line
+        }
     }
-}
 
-if($debugProject) {
-    [System.IO.File]::WriteAllLines("Couchbase.Lite.Tests.NetCore.Source.csproj", $content)
-} else {
     [System.IO.File]::WriteAllLines("Couchbase.Lite.Tests.NetCore.csproj", $content)
-}
+} else {
+    $scriptpath = Split-Path $MyInvocation.MyCommand.Path
+    Push-Location $scriptpath
+    [Environment]::CurrentDirectory = $scriptpath
+
+    if($debugProject) {
+        $content = [System.IO.File]::ReadAllLines("Couchbase.Lite.Tests.NetCore.Source.csproj")
+    } else {
+        $content = [System.IO.File]::ReadAllLines("Couchbase.Lite.Tests.NetCore.csproj")
+    }
+
+    $regex = New-Object -TypeName "System.Text.RegularExpressions.Regex" ".*?<PackageReference Include=`"Couchbase.Lite.Enterprise.*?`" Version=`"(.*?)`""
+    for($i = 0; $i -lt $content.Length; $i++) {
+        $line = $content[$i]
+        $matches = $regex.Matches($line)
+        if($matches.Count -gt 0) {
+            $oldVersion = $matches[0].Groups[1]
+            $line = $line.Replace($oldVersion, $version)
+            $content[$i] = $line
+        }
+    }
+
+    if($debugProject) {
+        [System.IO.File]::WriteAllLines("Couchbase.Lite.Tests.NetCore.Source.csproj", $content)
+    } else {
+        [System.IO.File]::WriteAllLines("Couchbase.Lite.Tests.NetCore.csproj", $content)
+    }
 }
--- src/Couchbase.Lite.Tests.UWP/modify_packages.ps1
+++ src/Couchbase.Lite.Tests.UWP/modify_packages.ps1
@@ -2,30 +2,51 @@ param (
     [Parameter(Mandatory=$true)][string]$version,
     [switch]$debugProject
 )
+$sourceProjectFile = "Couchbase.Lite.Tests.NetCore.Source.csproj"
+if (-NOT (Test-Path $sourceProjectFile)) {
+    $scriptpath = Split-Path $MyInvocation.MyCommand.Path
+    Push-Location $scriptpath
+    [Environment]::CurrentDirectory = $scriptpath
 
-$scriptpath = Split-Path $MyInvocation.MyCommand.Path
-Push-Location $scriptpath
-[Environment]::CurrentDirectory = $scriptpath
-
-if($debugProject){
     $content = [System.IO.File]::ReadAllLines("Couchbase.Lite.Tests.UWP.csproj")
-} else {
-    $content = [System.IO.File]::ReadAllLines("Couchbase.Lite.Tests.UWP.Source.csproj")
-}
 
-$regex = New-Object -TypeName "System.Text.RegularExpressions.Regex" ".*?<Version>([0-9]\.[0-9]\.[0-9]-b[0-9]{4})<.*?"
-for($i = 0; $i -lt $content.Length; $i++) {
-    $line = $content[$i]
-    $matches = $regex.Matches($line)
-    if($matches.Count -gt 0) {
-        $oldVersion = $matches[0].Groups[1]
-        $line = $line.Replace($oldVersion, $version)
-        $content[$i] = $line
+    $regex = New-Object -TypeName "System.Text.RegularExpressions.Regex" ".*?<Version>([0-9]\.[0-9]\.[0-9]-b[0-9]{4})<.*?"
+    for($i = 0; $i -lt $content.Length; $i++) {
+        $line = $content[$i]
+        $matches = $regex.Matches($line)
+        if($matches.Count -gt 0) {
+            $oldVersion = $matches[0].Groups[1]
+            $line = $line.Replace($oldVersion, $version)
+            $content[$i] = $line
+        }
     }
-}
 
-if($debugProject){
     [System.IO.File]::WriteAllLines("Couchbase.Lite.Tests.UWP.csproj", $content)
 } else {
-    [System.IO.File]::WriteAllLines("Couchbase.Lite.Tests.UWP.Source.csproj", $content)
+    $scriptpath = Split-Path $MyInvocation.MyCommand.Path
+    Push-Location $scriptpath
+    [Environment]::CurrentDirectory = $scriptpath
+
+    if($debugProject){
+        $content = [System.IO.File]::ReadAllLines("Couchbase.Lite.Tests.UWP.csproj")
+    } else {
+        $content = [System.IO.File]::ReadAllLines("Couchbase.Lite.Tests.UWP.Source.csproj")
+    }
+
+    $regex = New-Object -TypeName "System.Text.RegularExpressions.Regex" ".*?<Version>([0-9]\.[0-9]\.[0-9]-b[0-9]{4})<.*?"
+    for($i = 0; $i -lt $content.Length; $i++) {
+        $line = $content[$i]
+        $matches = $regex.Matches($line)
+        if($matches.Count -gt 0) {
+            $oldVersion = $matches[0].Groups[1]
+            $line = $line.Replace($oldVersion, $version)
+            $content[$i] = $line
+        }
+    }
+
+    if($debugProject){
+        [System.IO.File]::WriteAllLines("Couchbase.Lite.Tests.UWP.csproj", $content)
+    } else {
+        [System.IO.File]::WriteAllLines("Couchbase.Lite.Tests.UWP.Source.csproj", $content)
+    }
 }
