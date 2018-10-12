ef54be83578fe78210916f6637d38d323ac2cea2
Jim Borden <jim.borden@couchbase.com>
2018-10-12 10:06:19 +0900

    Update modify packages script
    It wasn’t having any effect on the master branch

--- src/Couchbase.Lite.Tests.Android/modify_packages.sh
+++ src/Couchbase.Lite.Tests.Android/modify_packages.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-if [ -f Couchbase.Lite.Tests.Android.Source.csproj ];
 pushd `dirname $0`
-then
 sed "s/[0-9].[0-9].[0-9]-b..../$1-$2/g" Couchbase.Lite.Tests.Android.csproj > tmp
 mv tmp Couchbase.Lite.Tests.Android.csproj
-sed "s/[0-9].[0-9].[0-9]-b..../$1-$2/g" Couchbase.Lite.Tests.Android.Source.csproj > tmp
-mv tmp Couchbase.Lite.Tests.Android.Source.csproj
 popd
-fi
--- src/Couchbase.Lite.Tests.iOS/modify_packages.sh
+++ src/Couchbase.Lite.Tests.iOS/modify_packages.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-if [ -f Couchbase.Lite.Tests.iOS.Source.csproj ];
 pushd `dirname $0`
-then
 sed "s/[0-9].[0-9].[0-9]-b..../$1-$2/g" Couchbase.Lite.Tests.iOS.csproj > tmp
 mv tmp Couchbase.Lite.Tests.iOS.csproj
-sed "s/[0-9].[0-9].[0-9]-b..../$1-$2/g" Couchbase.Lite.Tests.iOS.Source.csproj > tmp
-mv tmp Couchbase.Lite.Tests.iOS.Source.csproj
 popd
-fi
