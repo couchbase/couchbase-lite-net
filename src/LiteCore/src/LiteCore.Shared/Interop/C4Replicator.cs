678553689aa7c6e298a7b26e8c0d877fd47eb51b
Jim Borden <jim.borden@couchbase.com>
2019-01-30 10:26:03 +0900

    Work around the slippery lifetime of a struct
    Comments in the code, but basically C4Replicator needs to keep a copy of the SocketFactory struct around instead of just the pointer it has in its properties

--- src/LiteCore/src/LiteCore.Shared/Interop/C4Replicator.cs
+++ src/LiteCore/src/LiteCore.Shared/Interop/C4Replicator.cs
@@ -19,11 +19,9 @@
 using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
-using System.Threading;
 
 using LiteCore.Interop;
 using LiteCore.Util;
-using ObjCRuntime;
 
 namespace LiteCore.Interop
 {
@@ -53,68 +51,21 @@ namespace Couchbase.Lite.Interop
 {
     internal sealed class ReplicatorParameters : IDisposable
     {
+        #region Variables
+
         private C4ReplicatorParameters _c4Params;
+        private C4SocketFactory _factoryKeepAlive;
+        private bool _hasFactory;
+        private C4ReplicatorDocumentEndedCallback _onDocumentEnded;
         private C4ReplicatorStatusChangedCallback _onStatusChanged;
         private C4ReplicatorValidationFunction _pushFilter;
         private C4ReplicatorValidationFunction _validation;
-        private C4ReplicatorDocumentEndedCallback _onDocumentEnded;
+
+        #endregion
+
+        #region Properties
 
         public C4ReplicatorParameters C4Params => _c4Params;
-
-        public ReplicatorParameters(IDictionary<string, object> options)
-        {
-            if (options != null) {
-                _c4Params.optionsDictFleece = (FLSlice) options.FLEncode();
-            }
-        }
-
-        public C4ReplicatorMode Push
-        {
-            get => _c4Params.push;
-            set => _c4Params.push = value;
-        }
-
-        public C4ReplicatorMode Pull
-        {
-            get => _c4Params.pull;
-            set => _c4Params.pull = value;
-        }
-
-        public C4ReplicatorStatusChangedCallback OnStatusChanged
-        {
-            get => _onStatusChanged;
-            set {
-                _onStatusChanged = value;
-                _c4Params.onStatusChanged = Marshal.GetFunctionPointerForDelegate(value);
-            }
-        }
-
-        public C4ReplicatorDocumentEndedCallback OnDocumentEnded
-        {
-            get => _onDocumentEnded;
-            set {
-                _onDocumentEnded = value;
-                _c4Params.onDocumentEnded = Marshal.GetFunctionPointerForDelegate(value);
-            }
-        }
-
-        public C4ReplicatorValidationFunction PushFilter
-        {
-            get => _pushFilter;
-            set {
-                _pushFilter = value;
-                _c4Params.pushFilter = Marshal.GetFunctionPointerForDelegate(value);
-            }
-        }
-
-        public C4ReplicatorValidationFunction PullFilter
-        {
-            get => _validation;
-            set {
-                _validation = value;
-                _c4Params.validationFunc = Marshal.GetFunctionPointerForDelegate(value);
-            }
-        }
 
         public unsafe object Context
         {
@@ -130,21 +81,121 @@ namespace Couchbase.Lite.Interop
             }
         }
 
+        public C4ReplicatorDocumentEndedCallback OnDocumentEnded
+        {
+            get => _onDocumentEnded;
+            set {
+                _onDocumentEnded = value;
+                _c4Params.onDocumentEnded = Marshal.GetFunctionPointerForDelegate(value);
+            }
+        }
+
+        public C4ReplicatorStatusChangedCallback OnStatusChanged
+        {
+            get => _onStatusChanged;
+            set {
+                _onStatusChanged = value;
+                _c4Params.onStatusChanged = Marshal.GetFunctionPointerForDelegate(value);
+            }
+        }
+
+        public C4ReplicatorMode Pull
+        {
+            get => _c4Params.pull;
+            set => _c4Params.pull = value;
+        }
+
+        public C4ReplicatorValidationFunction PullFilter
+        {
+            get => _validation;
+            set {
+                _validation = value;
+                _c4Params.validationFunc = Marshal.GetFunctionPointerForDelegate(value);
+            }
+        }
+
+        public C4ReplicatorMode Push
+        {
+            get => _c4Params.push;
+            set => _c4Params.push = value;
+        }
+
+        public C4ReplicatorValidationFunction PushFilter
+        {
+            get => _pushFilter;
+            set {
+                _pushFilter = value;
+                _c4Params.pushFilter = Marshal.GetFunctionPointerForDelegate(value);
+            }
+        }
+
         public unsafe C4SocketFactory* SocketFactory
         {
             get => _c4Params.socketFactory;
-            set => _c4Params.socketFactory = value;
+            set {
+                if (_hasFactory) {
+                    // Unlikely, but here for safety.  This means that the SocketFactory
+                    // was already set before, but being reset.  So free the old context.
+                    GCHandle.FromIntPtr((IntPtr)_factoryKeepAlive.context).Free();
+                }
+
+                _c4Params.socketFactory = value;
+                _hasFactory = value != null;
+                if (_hasFactory) {
+                    // HACK: This object will not survive long because it is a struct
+                    // but the property type is a pointer.  Keep the actual object here
+                    // so that it can be disposed later
+                    _factoryKeepAlive = *value;
+                }
+            }
         }
 
-        public void Dispose()
+        #endregion
+
+        #region Constructors
+
+        public ReplicatorParameters(IDictionary<string, object> options)
+        {
+            if (options != null) {
+                _c4Params.optionsDictFleece = (FLSlice) options.FLEncode();
+            }
+        }
+
+        ~ReplicatorParameters()
+        {
+            Dispose(true);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private unsafe void Dispose(bool finalizing)
         {
             Native.FLSliceResult_Release((FLSliceResult)_c4Params.optionsDictFleece);
             Context = null;
+            if (_hasFactory) {
+                GCHandle.FromIntPtr((IntPtr)_factoryKeepAlive.context).Free();
+            }
         }
+
+        #endregion
+
+        #region IDisposable
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
     }
 
     internal static unsafe partial class Native
     {
+        #region Public Methods
+
         public static C4Replicator* c4repl_new(C4Database* db, C4Address remoteAddress, string remoteDatabaseName,
             C4Database *otherDb, C4ReplicatorParameters @params, C4Error* err)
         {
@@ -152,5 +203,7 @@ namespace Couchbase.Lite.Interop
                 return c4repl_new(db, remoteAddress, remoteDatabaseName_.AsFLSlice(), otherDb, @params, err);
             }
         }
+
+        #endregion
     }
 }
