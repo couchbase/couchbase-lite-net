cd96901ac217f24832c57b81d6ccb1c83f4615ba
Sandy C <sandy.chuang@couchbase.com>
2019-05-26 00:28:37 -0700

    Update the purge doc by doc to throw not found error when trying to purge the doc no longer existed and add a test to verify.

--- src/Couchbase.Lite.Shared/API/Database/Database.cs
+++ src/Couchbase.Lite.Shared/API/Database/Database.cs
@@ -679,26 +679,7 @@ namespace Couchbase.Lite
         public void Purge([NotNull]Document document)
         {
             CBDebug.MustNotBeNull(WriteLog.To.Database, Tag, nameof(document), document);
-
-            ThreadSafety.DoLocked(() =>
-            {
-                CheckOpen();
-                VerifyDB(document);
-
-                if (!document.Exists) {
-                    throw new CouchbaseLiteException(C4ErrorCode.NotFound);
-                }
-
-                InBatch(() =>
-                {
-                    var result = Native.c4doc_purgeRevision(document.c4Doc.RawDoc, null, null);
-                    if (result >= 0) {
-                        LiteCoreBridge.Check(err => Native.c4doc_save(document.c4Doc.RawDoc, 0, err));
-                    }
-                });
-
-                document.ReplaceC4Doc(null);
-            });
+            InBatch(() => PurgeDocById(document.Id));
         }
 
         /// <summary>
