c1fcf334c5787b2f744340a6a8aa344a39eff7f1
Jim Borden <jim.borden@couchbase.com>
2019-09-13 05:13:43 +0000

    Update do_clean.ps1
    Use dotnet CLI because nuget cannot always be found

--- packaging/nuget/do_clean.ps1
+++ packaging/nuget/do_clean.ps1
@@ -13,10 +13,10 @@ foreach($result in $results) {
     $now = $(Get-Date).ToUniversalTime()
     $limit = New-TimeSpan -Days 30
     if(($now - $published) -gt $limit) {
-        echo "Deleting $($result.Id)-$($result.Version)"
+        Write-Host "Deleting $($result.Id)-$($result.Version)"
         
         # Nuget won't fail when deleting a non-existent package, and Internal is a strict subset of CI
-        nuget delete -ApiKey $apikey -Source http://mobile.nuget.couchbase.com/nuget/CI -NonInteractive $result.Id $result.Version
-        nuget delete -ApiKey $apikey -Source http://mobile.nuget.couchbase.com/nuget/Internal -NonInteractive $result.Id $result.Version
+        dotnet nuget delete --api-key $apikey --source http://mobile.nuget.couchbase.com/nuget/CI --non-interactive $result.Id $result.Version
+        dotnet nuget delete --api-key $apikey --source http://mobile.nuget.couchbase.com/nuget/Internal --non-interactive $result.Id $result.Version
     }
 }
