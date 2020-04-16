3c631830b3c108646e72d2b5cf35ef16eb95701e
Sandy <sandy.chuang@couchbase.com>
2020-04-16 15:34:50 -0700

    Enhance Database Close

--- src/Couchbase.Lite.Shared/Query/XQuery.cs
+++ src/Couchbase.Lite.Shared/Query/XQuery.cs
@@ -293,6 +293,8 @@ namespace Couchbase.Lite.Internal.Query
                 return;
             }
 
+            _updating.Set(true);
+
             // External updates should poll less frequently
             var updateInterval = _updateInterval;
 
