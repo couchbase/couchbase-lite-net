d2cc8dbbc27f992b34313fb7b69492e061e2b041
Sandy <sandy.chuang@couchbase.com>
2020-08-25 13:42:49 -0700

    Make sure Db and OtherDb are free and clean before test delete and close WithActiveReplicatorAndURLEndpointListeners.

--- src/Couchbase.Lite.Tests.Shared/URLEndpointListenerTest.cs
+++ src/Couchbase.Lite.Tests.Shared/URLEndpointListenerTest.cs
@@ -776,6 +776,10 @@ namespace Test
 
         private void WithActiveReplicatorAndURLEndpointListeners(bool isCloseNotDelete)
         {
+            Db.Delete();
+            ReopenDB();
+
+            ReopenOtherDb();
             WaitAssert waitIdleAssert1 = new WaitAssert();
             WaitAssert waitStoppedAssert1 = new WaitAssert();
 
