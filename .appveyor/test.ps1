7d08c25897a09b40458ad5d08e2f670e0c2a0a9d
Sandy <sandy.chuang@couchbase.com>
2020-12-16 23:03:07 -0800

    Change to xunit.runner.console.2.4.1

--- .appveyor/test.ps1
+++ .appveyor/test.ps1
@@ -1,7 +1,7 @@
 dotnet tool install coveralls.net --version 1.0.0 --tool-path src\Couchbase.Lite.TestCoverage\coveralls
 Push-Location $PSScriptRoot\..\src\Couchbase.Lite.TestCoverage\
 Push-Location bin\Debug\netcoreapp2.1
-& ..\..\..\packages\OpenCover.4.7.922\tools\OpenCover.Console.exe -returntargetcode -register:user -target:dotnet.exe "-targetargs:""..\..\..\packages\xunit.runner.console.2.3.1\tools\netcoreapp2.1\xunit.console.dll"" "".\Couchbase.Lite.TestCoverage.dll"" -noshadow -appveyor"  -oldStyle -output:opencoverCoverage.xml -filter:"+[Couchbase.Lite]* -[Couchbase.Lite]LiteCore.Interop.*" -excludebyattribute:"*.ExcludeFromCodeCoverageAttribute" 2> $null;
+& ..\..\..\packages\OpenCover.4.7.922\tools\OpenCover.Console.exe -returntargetcode -register:user -target:dotnet.exe "-targetargs:""..\..\..\packages\xunit.runner.console.2.4.1\tools\netcoreapp2.1\xunit.console.dll"" "".\Couchbase.Lite.TestCoverage.dll"" -noshadow -appveyor"  -oldStyle -output:opencoverCoverage.xml -filter:"+[Couchbase.Lite]* -[Couchbase.Lite]LiteCore.Interop.*" -excludebyattribute:"*.ExcludeFromCodeCoverageAttribute" 2> $null;
 if($LASTEXITCODE -ne 0) {
     Pop-Location
     exit $LASTEXITCODE
