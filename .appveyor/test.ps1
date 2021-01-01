578f18332a524cf6c4055c336817d37142b134ea
Sandy <sandy.chuang@couchbase.com>
2020-12-31 19:30:57 -0800

    Remove installing coveralls.net

--- .appveyor/test.ps1
+++ .appveyor/test.ps1
@@ -1,4 +1,3 @@
-dotnet tool install coveralls.net --version 1.0.0 --tool-path src\Couchbase.Lite.TestCoverage\coveralls
 Push-Location $PSScriptRoot\..\src\Couchbase.Lite.TestCoverage\
 dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura
 Pop-Location
