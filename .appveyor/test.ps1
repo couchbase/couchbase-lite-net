cbff88e4aa2e02ea351c845c7558466c8783ca5a
Sandy <sandy.chuang@couchbase.com>
2020-12-31 21:18:12 -0800

    Add coverlet nuget packages directly to csproj

--- .appveyor/test.ps1
+++ .appveyor/test.ps1
@@ -1,3 +1,12 @@
 Push-Location $PSScriptRoot\..\src\Couchbase.Lite.TestCoverage\
 dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura
+if($LASTEXITCODE -ne 0) {
+    exit $LASTEXITCODE
+}
+
+if (-not (Test-Path env:APPVEYOR_PULL_REQUEST_NUMBER)) {
+    Pop-Location
+    exit $LASTEXITCODE
+}
+
 Pop-Location
