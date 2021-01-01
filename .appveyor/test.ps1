54c780b7024b27cfa81c0f6cb96a2e790a3360a0
Sandychuang8 <sandy.chuang@couchbase.com>
2020-12-31 21:43:39 -0800

    CBL-1118 Update SimpleInjector version to 5+ (#1264)
    * Update SimpleInjector version to 5+
    
    * Use Coverlet

--- .appveyor/test.ps1
+++ .appveyor/test.ps1
@@ -1,15 +1,10 @@
-dotnet tool install coveralls.net --version 1.0.0 --tool-path src\Couchbase.Lite.TestCoverage\coveralls
 Push-Location $PSScriptRoot\..\src\Couchbase.Lite.TestCoverage\
-Push-Location bin\Debug\netcoreapp2.1
-& ..\..\..\packages\OpenCover.4.7.922\tools\OpenCover.Console.exe -returntargetcode -register:user -target:dotnet.exe "-targetargs:""..\..\..\packages\xunit.runner.console.2.3.1\tools\netcoreapp2.1\xunit.console.dll"" "".\Couchbase.Lite.TestCoverage.dll"" -noshadow -appveyor"  -oldStyle -output:opencoverCoverage.xml -filter:"+[Couchbase.Lite]* -[Couchbase.Lite]LiteCore.Interop.*" -excludebyattribute:"*.ExcludeFromCodeCoverageAttribute" 2> $null;
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura
 if($LASTEXITCODE -ne 0) {
-    Pop-Location
     exit $LASTEXITCODE
 }
 
-Pop-Location
 if (-not (Test-Path env:APPVEYOR_PULL_REQUEST_NUMBER)) {
-    & "coveralls\csmacnz.coveralls.exe" --opencover -i "bin\Debug\netcoreapp2.1\opencoverCoverage.xml"
     Pop-Location
     exit $LASTEXITCODE
 }
