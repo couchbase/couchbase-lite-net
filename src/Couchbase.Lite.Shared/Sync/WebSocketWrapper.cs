4cf3f4b0c571bcce7eb7130a52a3ebad55f9faac
Sandy <sandy.chuang@couchbase.com>
2021-07-26 21:33:07 -0700

    Try catch more host connection exceptions and handle them if any
    (cherry picked from commit 9ae0b3f4ed4f8a714a9d8539b971a582488b6960)

--- src/Couchbase.Lite.Shared/Sync/WebSocketWrapper.cs
+++ src/Couchbase.Lite.Shared/Sync/WebSocketWrapper.cs
@@ -424,6 +424,11 @@ namespace Couchbase.Lite.Sync
                 return false;
             }
 
+            if (!t.IsCompletedSuccessfully || t.IsFaulted) {
+                DidClose(new SocketException((int)SocketError.SocketError));
+                return false;
+            }
+
             return true;
         }
 
@@ -467,6 +472,7 @@ namespace Couchbase.Lite.Sync
                         if (!NetworkTaskSuccessful(t)) {
                             return;
                         }
+
                         _queue.DispatchAsync(StartInternal);
 
                     });
