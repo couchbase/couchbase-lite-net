f740e30029cf411d84e83c4efe80097985f258da
Sandy <sandy.chuang@couchbase.com>
2022-04-25 18:18:56 -0700

    Run both 3.1 and 6.0 tests in GHA

--- src/Couchbase.Lite.Shared/API/DI/Service.cs
+++ src/Couchbase.Lite.Shared/API/DI/Service.cs
@@ -57,7 +57,7 @@ namespace Couchbase.Lite.DI
         static Service()
         {
             // Windows 2012 doesn't define NETFRAMEWORK for some reason
-            #if NETCOREAPP3_1 || NETFRAMEWORK || NET461
+            #if NETCOREAPP3_1_OR_GREATER || NETCOREAPP || NETFRAMEWORK || NET461
             AutoRegister(typeof(Database).GetTypeInfo().Assembly);
             
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
