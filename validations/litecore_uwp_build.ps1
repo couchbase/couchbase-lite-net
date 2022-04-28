bbedc80fcaa155a94803ad4e172eab609e800205
Sandy <sandy.chuang@couchbase.com>
2022-04-28 13:29:31 -0700

    Test picking build framework

--- validations/litecore_uwp_build.ps1
+++ validations/litecore_uwp_build.ps1
@@ -12,7 +12,6 @@ Pop-Location
 
 New-Item -Type Directory couchbase-lite-net/vendor/couchbase-lite-core/build_cmake/x64_store
 Push-Location couchbase-lite-net/vendor/couchbase-lite-core/build_cmake/x64_store
-#cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION='10.0' ..\..
 cmake -G "Visual Studio 16 2019" -A x64 -DBUILD_ENTERPRISE=ON -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION='10.0' ..\..
 cmake --build . --target LiteCore --config RelWithDebInfo
 Pop-Location
