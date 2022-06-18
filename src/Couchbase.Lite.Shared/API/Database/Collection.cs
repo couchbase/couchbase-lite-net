36c56b609592e6af4c1da8b1e1482d70dcadc89e
Sandy <sandy.chuang@couchbase.com>
2022-06-17 20:02:49 -0700

    Make sure Collection methods are checking for collection is valid

--- src/Couchbase.Lite.Shared/API/Database/Collection.cs
+++ src/Couchbase.Lite.Shared/API/Database/Collection.cs
@@ -228,7 +228,6 @@ namespace Couchbase.Lite
             CBDebug.MustNotBeNull(WriteLog.To.Database, Tag, nameof(document), document);
             ThreadSafety.DoLocked(() =>
             {
-                CheckCollectionValid();
                 VerifyCollection(document);
 
                 if (!document.Exists) {
@@ -250,7 +249,6 @@ namespace Couchbase.Lite
         public void Purge([NotNull] string docId)
         {
             CBDebug.MustNotBeNull(WriteLog.To.Database, Tag, nameof(docId), docId);
-            CheckCollectionValid();
             Database.InBatch(() => PurgeDocById(docId));
         }
 
