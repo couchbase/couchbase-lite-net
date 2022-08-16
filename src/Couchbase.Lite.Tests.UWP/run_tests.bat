89595338551744b5d1e44d7709181ce462cb8b43
Sandy <sandy.chuang@couchbase.com>
2022-08-15 23:53:38 -0700

    Update run_test.bat

--- src/Couchbase.Lite.Tests.UWP/run_tests.bat
+++ src/Couchbase.Lite.Tests.UWP/run_tests.bat
@@ -7,7 +7,8 @@ rmdir /s /q AppPackages
 if "%1" == "Debug" (
     msbuild /t:Restore Couchbase.Lite.Tests.UWP.csproj
     msbuild /p:Configuration=Debug /p:Platform=x64 /t:Rebuild Couchbase.Lite.Tests.UWP.csproj || goto :error
-    vstest.console.exe /InIsolation /Platform:x64 AppPackages\Couchbase.Lite.Tests.UWP_1.0.0.0_x64_Debug_Test\Couchbase.Lite.Tests.UWP_1.0.0.0_x64_Debug.appx /Logger:trx /diag:diagnostic.txt || goto :error
+    ::vstest.console.exe /InIsolation /Platform:x64 AppPackages\Couchbase.Lite.Tests.UWP_1.0.0.0_x64_Debug_Test\Couchbase.Lite.Tests.UWP_1.0.0.0_x64_Debug.appx /Logger:trx /diag:diagnostic.txt || goto :error
+	vstest.console.exe /InIsolation /Platform:x64 bin\x64\Debug\Couchbase.Lite.Tests.UWP.build.appxrecipe /Logger:trx /diag:diagnostic.txt /Framework:FrameworkUap10 || goto :error
     goto :EOF
 ) else (
     msbuild /t:Restore /p:Configuration=Release Couchbase.Lite.Tests.UWP.csproj
