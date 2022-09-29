5385dbca0f577c050ea3ebf64c1dd7baa38f92e5
Sandychuang8 <sandy.chuang@couchbase.com>
2022-09-28 18:32:55 -0700

    CBL-3652 pac file support in android system proxy auto-config (#1431)
    (cherry picked from commit 06c11dbbd41b2d2db244a3cccf948a26e88e5c01)

--- src/Couchbase.Lite.Shared/Support/Android/XamarinAndroidProxy.cs
+++ src/Couchbase.Lite.Shared/Support/Android/XamarinAndroidProxy.cs
@@ -17,12 +17,13 @@
 // 
 #if __ANDROID__
 using System;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 
 using Couchbase.Lite.DI;
 using Couchbase.Lite.Internal.Logging;
-using Java.Lang;
+using Java.Net;
 
 namespace Couchbase.Lite.Support
 {
@@ -32,22 +33,42 @@ namespace Couchbase.Lite.Support
 
         public Task<WebProxy> CreateProxyAsync(Uri destination)
         {
-            WebProxy webproxy = null;
-            // if a proxy is enabled set it up here
-            string host = JavaSystem.GetProperty("http.proxyHost")?.TrimEnd('/');
-            string port = JavaSystem.GetProperty("http.proxyPort");
-
-            try {
-                webproxy = new WebProxy(host, Int32.Parse(port));
-            } catch { // UriFormatException
-                WriteLog.To.Sync.W("CreateProxyAsync", "The URI formed by combining Host and Port is not a valid URI. Please check your system proxy setting.");
-                return Task.FromResult<WebProxy>(null);
+            WebProxy webProxy = null;
+            var selector = ProxySelector.Default;
+            if (selector != null) {
+                try {
+                    var javaUri = new URI(EncodeUrl(destination));
+                    var uriSelector = selector.Select(javaUri);
+                    var proxy = uriSelector.FirstOrDefault();
+                    if (proxy != null && proxy != Proxy.NoProxy && proxy.Address() is InetSocketAddress address) {
+                        webProxy = new WebProxy(address.HostString, address.Port);
+                    }
+                } catch { // UriFormatException
+                    WriteLog.To.Sync.W("CreateProxyAsync", "The URI formed by combining Host and Port is not a valid URI. Please check your system proxy setting.");
+                }
             }
 
-            return Task.FromResult(webproxy);
+            return Task.FromResult(webProxy);
         }
 
         #endregion
+
+        private string EncodeUrl(Uri url)
+        {
+            // Copied from https://github.com/xamarin/xamarin-android/blob/master/src/Mono.Android/Xamarin.Android.Net/AndroidClientHandler.cs
+            // Fixes an issue where urls with unencoded spaces are not recognized by the Java URI class.
+
+            if (url == null)
+                return string.Empty;
+
+            // UriBuilder takes care of encoding everything properly
+            var bldr = new UriBuilder(url);
+            if (url.IsDefaultPort)
+                bldr.Port = -1; // Avoids adding :80 or :443 to the host name in the result
+
+            // bldr.Uri.ToString () would ruin the good job UriBuilder did
+            return bldr.ToString();
+        }
     }
 }
 #endif
