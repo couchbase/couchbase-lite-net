ca2c49cf2d2ceede25a79bcc565aa0bd908075b5
Sandychuang8 <sandy.chuang@couchbase.com>
2022-09-30 21:29:15 -0700

    Increase wait on TestExpiredNotInQuery

--- src/Couchbase.Lite.Tests.Shared/QueryTest.cs
+++ src/Couchbase.Lite.Tests.Shared/QueryTest.cs
@@ -256,7 +256,7 @@ namespace Test
             }
             
             Db.SetDocumentExpiration(docId, DateTimeOffset.Now);
-            Thread.Sleep(5);
+            Thread.Sleep(50);
 
             using (var doc2 = new MutableDocument("doc2")) {
                 doc2.SetString("expire_me", "never");
