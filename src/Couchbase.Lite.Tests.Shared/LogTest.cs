767e186aec8beaf31ca5bc9499029211af5b79a0
Sandychuang8 <sandy.chuang@couchbase.com>
2022-12-12 17:44:35 -0800

    CBL-3898 Add Maui Test project as part of PR validations (Windows) (#1454)

--- src/Couchbase.Lite.Tests.Shared/LogTest.cs
+++ src/Couchbase.Lite.Tests.Shared/LogTest.cs
@@ -51,7 +51,7 @@ namespace Test
     #endif
     public sealed class LogTest
     {
-        #if NETCOREAPP3_1_OR_GREATER && !CBL_NO_VERSION_CHECK && !NET6_0_WINDOWS10 && !__ANDROID__ && !__IOS__
+        #if NETCOREAPP3_1_OR_GREATER && !CBL_NO_VERSION_CHECK && !NET6_0_WINDOWS10 && !__ANDROID__ && !__IOS__ && !WINUI
         static LogTest()
         {
             Couchbase.Lite.Support.NetDesktop.CheckVersion();
