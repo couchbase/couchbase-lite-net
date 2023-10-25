a4d64aa3f88cf1e497555ed54527fb2dfa5c7a84
G. Blake Meike <blake.meike@gmail.com>
2023-10-25 12:51:49 -0700

    CBL-4983: Map LiteCore log domain 'Changes' to Database instead of Replicator (#1573)

--- src/Couchbase.Lite.Shared/Log/LogTo.cs
+++ src/Couchbase.Lite.Shared/Log/LogTo.cs
@@ -205,7 +205,7 @@ namespace Couchbase.Lite.Internal.Logging
             ["Sync"] = LogDomain.Replicator,
             ["SyncBusy"] = LogDomain.Replicator,
             ["Actor"] = LogDomain.Replicator,
-            ["Changes"] = LogDomain.Replicator,
+            ["Changes"] = LogDomain.Database,
             ["Query"] = LogDomain.Query,
             ["Enum"] = LogDomain.Query,
             ["WS"] = LogDomain.Network,
